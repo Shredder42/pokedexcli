34bc309fee459bb342fb95cc0309ac30efa6ad41
shredder42 <atbinder42@gmail.com>
2025-04-23 11:02:51 -0500

    start repl

--- repl.go
+++ repl.go
@@ -1,8 +1,27 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
+	"os"
 	"strings"
 )
+
+func startRepl() {
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Print("Pokedex > ")
+		scanner.Scan()
+		text := scanner.Text()
+		cleanedText := cleanInput(text)
+		if len(cleanedText) == 0 {
+			continue
+		}
+
+		fmt.Printf("Your command was: %s\n", cleanedText[0])
+	}
+}
 
 func cleanInput(text string) []string {
 	editedWords := []string{}
